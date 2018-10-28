4ec63e12a37bfb12e04dca2454b39c5414f38fc6
Joshua <joshuacroft@trentu.ca>
2018-10-28 19:06:41 -0400

    -Added Barycentric class and both static conversion methods.

---
+++ src/Barycentric.java
@@ -0,0 +1,10 @@
+public class Barycentric {
+
+    public static Point3D convertFromCartesian(Point3D point) {
+        return point;
+    }
+
+    public static Point3D convertToCartesian(Point3D point) {
+        return point;
+    }
+}
